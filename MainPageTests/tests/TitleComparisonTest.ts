be1ae4132a4285c90b1511bea7b31517d13255bb
QA_Nikita_L <nikita.lysenko@abz.agency>
2024-06-30 12:16:19 +0300

    TitleComparisonTest added

---
+++ MainPageTests/tests/TitleComparisonTest.ts
@@ -0,0 +1,20 @@
+import {test,expect} from '@playwright/test'
+
+var testUrl = 'https://example.com'
+
+test("example test", async ({page})=>{
+await page.goto(testUrl)
+const pageTitle = await page.locator('h1')
+await expect(pageTitle).toContainText('Example Domain')
+})
+
+//to run the test with browswer launching
+// npx playwright test --headed
+//to run the test in chosen browser
+// npx playwright test --browser=firefox
+//to run the test in all browsers
+// npx playwright test --browser=all
+//example with combination
+// npx playwright test --headed --browser=firefox
+//to run specific test
+// npx playwright test tests/path.to.test
